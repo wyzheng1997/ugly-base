07b04cfd7e95787d0fb4cf4da3c0578f6935ee8a
wyzheng <wyzheng1997@163.com>
2023-11-02 11:56:28 +0800

    perf(辅助函数): arr2tree

--- src/helpers.php
+++ src/helpers.php
@@ -1,20 +1,14 @@
 <?php
 
-use Illuminate\Support\Collection;
-
 if (! function_exists('arr2tree')) {
     /**
      * 数组/集合 转换成树级结构.
      */
-    function arr2tree($list, Closure $transform = null,
-        string $id = 'id', string $pid = 'pid', string $children = 'children'): array
+    function arr2tree(array $list, Closure $transform = null, string $id = 'id', string $pid = 'pid', string $children = 'children'): array
     {
         [$map, $tree] = [[], []];
         foreach ($list as $item) {
             $map[data_get($item, $id)] = $transform ? call_user_func($transform, $item) : $item;
-        }
-        if ($list instanceof Collection) { // 集合转数组
-            $list = $list->toArray();
         }
 
         foreach ($list as $item) {
